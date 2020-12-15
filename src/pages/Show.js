9ceab3ea3fe44ad7603b2cf716b3a78af22a0d00
Dhanush <dhanuraj7258@gmail.com>
2020-12-15 11:54:33 +0530

    added => replaced useState with useReducer

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,23 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
+
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null,
+};
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, show: action.show, error: null };
+    }
+    case 'FETCH_FAILED': {
+      return { isLoading: true, error: action.error };
+    }
+
+    default:
+      return prevState;
+  }
+};
 
 // eslint-disable-next-line arrow-body-style
 const Show = () => {
   const { id } = useParams();
-  const [show, setShow] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  //   const [show, setShow] = useState(null);
+  //   const [isLoading, setIsLoading] = useState(true);
+  //   const [error, setError] = useState(null);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
     let isMounted = true;
     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then(data => {
-        setShow(data);
-        setIsLoading(false);
+        dispatch({ type: 'FETCH_SUCCESS', show: data });
       })
       .catch(err => {
-        setError(err.message);
+        dispatch({ type: 'FETCH_FAILED', error: err.messages });
       });
     return () => {
       // eslint-disable-next-line no-unused-vars
@@ -25,15 +46,15 @@ const Show = () => {
     };
   }, [id]);
 
-  console.log('show', show);
+  console.log(state);
 
-  if (isLoading) {
-    return <div>Loading....</div>;
-  }
+  //   if (dispatch({ type: '' })) {
+  //     return <div>Loading....</div>;
+  //   }
 
-  if (error) {
-    return <div>Error occured: {error} </div>;
-  }
+  //   if (error) {
+  //     return <div>Error occured: {error} </div>;
+  //   }
 
   return <div>Show</div>;
 };
